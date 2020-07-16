47266f82f33ff58e29b06412adcb8a95f442b9d9
George Benjamin-Schonberger <george.benjamin@gmail.com>
2020-07-16 15:13:05 +0300

    - Startup directory fix - gitignore changes (Rider meta dir added)

--- src/Adeotek.NetworkMonitor.CLI/Program.cs
+++ src/Adeotek.NetworkMonitor.CLI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -10,58 +11,71 @@ namespace Adeotek.NetworkMonitor.CLI
 {
     class Program
     {
+        private static IConfiguration _configuration;
+        private static ILogger _logger;
+        
         static void Main(string[] args)
         {
-            IConfiguration configuration;
-            ILogger logger;
-            var environmentName = Environment.GetEnvironmentVariable("DOTNET_CORE_ENVIRONMENT");
-
-            try
+            if (!LoadConfiguration(args))
             {
-                configuration = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                    .AddEnvironmentVariables()
-                    .AddCommandLine(args)
-                    .Build();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Unable to load configuration: {e.Message}");
                 return;
             }
 
-            try
-            {
-                var sLogger = new LoggerConfiguration()
-                    .ReadFrom.Configuration(configuration)
-                    .CreateLogger();
-
-                var services = new ServiceCollection();
-                services.AddLogging(configure => configure.AddSerilog(sLogger, true));
-                var provider = services.BuildServiceProvider();
-                logger = provider.GetService<ILoggerFactory>().CreateLogger("Application");
-                logger.LogDebug("Logger initialized.");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Unable to create logger object: {e.Message}");
-                logger = null;
-            }
+            SetupLogger();
 
             try
             {
-                var networkMonitor = new NetworkMonitor(configuration, logger);
+                var networkMonitor = new NetworkMonitor(_configuration, _logger);
                 networkMonitor.RunAllTests();
             }
             catch (Exception e)
             {
-                logger?.LogError(e, "NetworkMonitor.RunAllTest failed!");
+                _logger?.LogError(e, "NetworkMonitor.RunAllTest failed!");
             }
 
-            if (environmentName == "Development")
+            if (_configuration.GetValue("environment",string.Empty) == "Development")
             {
                 Console.ReadKey();
+            }
+        }
+
+        private static bool LoadConfiguration(string[] args)
+        {
+            try
+            {
+                _configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .AddCommandLine(args)
+                    .Build();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unable to load configuration: {e.Message}");
+                return false;
+            }
+        }
+
+        private static void SetupLogger()
+        {
+            try
+            {
+                ServiceCollection services;
+                using var sLogger = new LoggerConfiguration().ReadFrom.Configuration(_configuration)
+                    .CreateLogger();
+                services = new ServiceCollection();
+                services.AddLogging(configure => configure.AddSerilog(sLogger, true));
+
+                var provider = services.BuildServiceProvider();
+                _logger = provider.GetService<ILoggerFactory>().CreateLogger("Application");
+                _logger.LogDebug("Logger initialized.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Unable to create logger object: {e.Message}");
+                _logger = null;
             }
         }
     }
